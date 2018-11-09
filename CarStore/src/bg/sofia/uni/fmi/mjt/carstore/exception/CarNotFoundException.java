14e5c33149d779fbdd7e994a6a1b4b488278dfbc
Ikbal Rasimov <44362017+Ikbal01@users.noreply.github.com>
2018-11-09 17:27:08 +0200

    Delete CarNotFoundException.java

--- CarStore/src/bg/sofia/uni/fmi/mjt/carstore/exception/CarNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.carstore.exception;
-
-public class CarNotFoundException extends RuntimeException {
-    public CarNotFoundException() {
-        super("CarNotFoundException");
-    }
-}
